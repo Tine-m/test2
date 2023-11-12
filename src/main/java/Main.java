3037bd09484637313d14b5c554d3139f724bff95
Tine-m <tinemarbjerg@gmail.com>
2023-11-12 17:30:40 +0100

    implement subclass Dog methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hej eat feature branch");
         System.out.println("Hej igen");
+        Dog dog = new Dog();
+        dog.makeSound();
     }
 }
