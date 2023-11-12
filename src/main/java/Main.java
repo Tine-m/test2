bffeffc93e546bc5d900bb0afc7bb19ac5250ba0
Tine-m <tinemarbjerg@gmail.com>
2023-11-12 16:54:20 +0100

    Test cat

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hej eat feature branch");
         System.out.println("Hej igen");
+        Cat cat = new Cat();
+        cat.eatFood("fish");
     }
 }
