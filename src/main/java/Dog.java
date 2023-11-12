ed49e918a3b1bbad574c0998db298c1fc637f451
Tine-m <tinemarbjerg@gmail.com>
2023-11-12 16:53:42 +0100

    Cat og Dog implemented

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,2 +1,11 @@
-public class Dog {
+public class Dog extends Animal {
+    @Override
+    public void makeSound() {
+
+    }
+
+    @Override
+    public boolean eatFood(String foodType) {
+        return false;
+    }
 }
